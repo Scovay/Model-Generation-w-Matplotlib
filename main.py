a8665a14173bee94f4910f5124f1d46f64d99e5d
Scovay Xu <scovay.xu@gmail.com>
2020-09-12 21:45:21 +0000

    Strated Graph Class and finished setupCurve function

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random as rand
+import time
+
+seed = time.ctime()
+rand.seed(seed)
+
 
 class Curves():
-	def __init__(self, plots = [0, 10], numOfPoints = 21, yIntercept = 0, slope = 2):
-		self.point1 = plots[0]
-		self.point2 = plots[1]
+	def __init__(self, plot1 = 0, plot2 = 10, numOfPoints = 21, yIntercept = 0, slope = 2):
+		self.point1 = plot1
+		self.point2 = plot2
 		self.numOfPoints = numOfPoints
 		self.yIntercept = float(yIntercept)
 		self.slope = slope
@@ -13,7 +19,7 @@ class Curves():
 		self.y = self.slope * self.x + self.yIntercept
 	def write(self, color = 'blue', marker = 'o'):
 		plt.scatter(self.x, self.y, c = color, marker = marker)
-		return marker
+		plt.show()
 
 class XML():
 	def __init__(self, x, y, marker, diameter = 10):
@@ -53,3 +59,24 @@ class XML():
 				f.write("\t\t</bndbox>\n")
 				f.write("\t</object>\n\n")
 			f.write("</annotation>")
+
+class Graph():
+	def __init__(self, color = "white", width = 10, height = 6):
+		plt.figure(figsize=(width,height),facecolor = color)
+	def setupCurve(self):
+		plot1 = int(input("Enter point 1: "))
+		plot2 = int(input("Enter point 2: "))
+		numOfPoints = int(input("Enter the number of points: "))
+		yIntercept = int(input("Enter the Y intercept: "))
+		slope = int(input("Enter the slope: "))
+		if not (plot1 or plot2 or numOfPoints or yIntercept or slope):
+			plot1 = 0
+			plot2 = rand.randint(10, 25)
+			numOfPoints = rand.randint(20, 50)
+			yIntercept = rand.randint(0, 10)
+			slope = rand.randint()
+			self.curve = Curves(plot1, plot2, numOfPoints, yIntercept, slope)
+		else:
+			self.curve = Curves(plot1, plot2, numOfPoints, yIntercept, slope)
+	def setupXML(self):
+		marker = input("Enter the marker code: ")
