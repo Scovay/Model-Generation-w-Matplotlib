801dc46645a963f5ac2c0c772221813e375fa3f8
Jonathan <scovay.xu@gmail.com>
2020-09-12 15:00:34 +0000

    Finished Curves Dunder init function

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plot
 
 class Curves():
-	def __init__(self, NumOfX = "21", Symbol = ""):
-		self.NumOfX = NumOfX
-		self.Symbol = Symbol
+	def __init__(self, plots = [0, 10], numOfPoints = 21, yIntercept = 0.0, slope = 2):
+		self.point1 = plots[0]
+		self.point2 = plots[1]
+		self.numOfPoints = numOfPoints
+		self.yIntercept = yIntercept
+		self.slope = slope
+	
