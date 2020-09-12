dd4db780884821978a8e39b94b3299e70b26ea38
Jonathan <scovay.xu@gmail.com>
2020-09-12 15:07:30 +0000

    Added and finished createAxis and write

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 
 class Curves():
-	def __init__(self, plots = [0, 10], numOfPoints = 21, yIntercept = 0.0, slope = 2):
+	def __init__(self, plots = [0, 10], numOfPoints = 21, yIntercept = 0, slope = 2):
 		self.point1 = plots[0]
 		self.point2 = plots[1]
 		self.numOfPoints = numOfPoints
-		self.yIntercept = yIntercept
+		self.yIntercept = float(yIntercept)
 		self.slope = slope
-	
+	def createAxis(self):
+		self.x = np.linspace(self.point1,self.point2, num = self.numOfPoints)
+		self.y = self.slope * self.x + self.yIntercept
+	def write(self, color = 'blue', marker = 'o'):
+		plt.scatter(self.x, self.y, c = color, marker = marker)
